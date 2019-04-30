9754b8a6cdb6ca6fcc44cb5d7938340c798a7eb4
devfabio <fabiohgoulart9@gmail.com>
2019-04-30 09:46:38 -0300

    Inclusão das classes base para reutilização.

--- src/main/java/pagina/PaginaBase.java
+++ src/main/java/pagina/PaginaBase.java
@@ -1,4 +1,33 @@
 package pagina;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class PaginaBase {
+
+    public WebDriver driver;
+    public WebDriverWait espera;
+
+    //Construtor
+    public PaginaBase (WebDriver driver, WebDriverWait esp){
+        this.driver = driver;
+        this.espera = esp;
+    }
+
+    //Metodo clicar
+    public void click (By caminhoElemento) {
+        driver.findElement(caminhoElemento).click();
+    }
+
+    //Inserir dados
+    public void inserirTexto (By caminhoElemento, String text) {
+        driver.findElement(caminhoElemento).sendKeys(text);
+    }
+
+    //Ler dados
+    public String lerTexto (By caminhoElemento) {
+        return driver.findElement(caminhoElemento).getText();
+    }
+
 }
--- src/test/java/tests/TesteBase.java
+++ src/test/java/tests/TesteBase.java
@@ -1,4 +1,38 @@
 package tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
 public class TesteBase {
+
+    public WebDriver driver;
+    public WebDriverWait espera;
+
+    public WebDriver getDriver(){
+        return driver;
+    }
+
+    @BeforeClass
+    public void configurar(){
+
+        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+
+        //Cria o driver. Todas as classes de teste usam esta.
+        driver = new ChromeDriver();
+
+        //Cria  espera.
+        espera = new WebDriverWait(driver,25);
+
+        //Maximize a janela
+        //driver.manage().window().maximize();
+    }
+
+    @AfterClass
+    public void terminar(){
+        // encerra o test
+        driver.quit();
+    }
 }
